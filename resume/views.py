e49864d7de513357c77065b61821c817e2b977dc
Vadim Khizhin <martin.faulther@yandex.ru>
2020-08-31 22:30:14 +0300

    Stage 3: Vacancies and Resumes pages

--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from . models import Resume
 
 # Create your views here.
+
+class ResumeView(ListView):
+    model = Resume
+    template_name = 'resume.html'
--- vacancy/views.py
+++ vacancy/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from . models import Vacancy
 # Create your views here.
+
+class VacanciesView(ListView):
+    model = Vacancy
+    template_name = 'vacancies.html'
